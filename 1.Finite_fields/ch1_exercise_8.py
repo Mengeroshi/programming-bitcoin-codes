62d180c74fcf41f1a8a97e4fbafb222e8939c068
mengeroshi <dpllgr@hotmail.com>
2019-11-11 13:06:54 -0600

    chapter 1 finished

---
+++ 1.Finite_fields/ch1_exercise_8.py
@@ -0,0 +1,12 @@
+print((2*(7**17))%19)
+print((7*(5**17))%19)
+
+prime = 31
+#3/24
+print((3*pow(24, prime-2,))%prime)
+#17** -3 
+print(pow(17, prime-4, prime))
+#(4**-4) * 11
+print(((pow(4, prime-5, prime))*11)%prime)
+
+print(pow(17, 27, 31))
