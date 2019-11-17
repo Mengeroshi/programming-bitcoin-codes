f5e0139f102cba7e8239c19825c95e5c9c9cec9c
mengeroshi <dpllgr@hotmail.com>
2019-11-17 15:19:34 -0600

    exercise 1 ch7 added

--- 6.Script/lel.py
+++ 6.Script/lel.py
@@ -7,10 +7,3 @@ script_pubkey = Script([sec, 0xac])
 script_sig = Script([sig])
 combined_script = script_sig + script_pubkey
 print(combined_script.evaluate(z))
-
-from script import Script
-
-script_pubkey = Script([0x76, 0x76, 0x95, 0x93, 0x56, 0x87])
-script_sig = Script([0x52])  # FILL THIS IN
-combined_script = script_sig + script_pubkey
-print(combined_script.evaluate(0))
