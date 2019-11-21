6c06da5d7d55a19c76eda78555309ed1229924bf
mengeroshi <dpllgr@hotmail.com>
2019-11-20 23:57:35 -0600

    exercise 1 ch 12 added

---
+++ 12.Bloom_filters/ch12_exercise_1.py
@@ -0,0 +1,8 @@
+from helper import hash160
+bit_field_size = 10
+bit_field = [0] * bit_field_size
+for item in (b'hello world', b'goodbye'):
+    h = hash160(item)
+    bit = int.from_bytes(h, 'big') % bit_field_size
+    bit_field[bit] = 1
+print(bit_field)
